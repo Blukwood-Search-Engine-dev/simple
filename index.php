4265f23c9cac0aec61d606c42e4ee76d31836bd7
blukwood <67267829+blukwood@users.noreply.github.com>
2020-06-22 16:12:25 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Search Engine</title>
+</head>
+<body>
+<form action='search.php' method='GET'>
+<center>
+<br>
+<br>
+<img src="logo.png" alt="Smiley face" vspace="60" align = "center" height="130" width="250">
+<br>
+<input type='text' size='95' name='search' placeholder='Search' autocomplete="off" ><br><br><br>
+<input type='submit' name='submit' value='Search'></br></br></br>
+</center>
+</form>
+</body>
+</html>
